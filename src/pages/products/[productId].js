e91e111163760041fd0f5a03e7ea229713da99b8
Charles Villard <c.d.villard@gmail.com>
2022-11-07 10:26:34 -0800

    lesson 7

--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link';
 import Image from 'next/image';
+import { CldOgImage } from 'next-cloudinary';
 
 import Layout from '@components/Layout';
 import Container from '@components/Container';
@@ -19,6 +20,38 @@ export default function Product({ product, ogImage }) {
         <meta property="og:description" content={`${product.name} on Space Jelly gear!`} />
         <meta property="og:type" content="article" />
       </Head>
+
+      <CldOgImage
+        src="https://user-images.githubusercontent.com/1045274/199742477-3a683f54-915b-463d-95a2-8ac584db7240.png"
+        deliveryType="fetch"
+        twitterTitle={`${product.name} - Space Jelly Gear`}
+        overlays={[
+          {
+            url: product.image,
+            crop: 'fill',
+            width: 1200,
+            height: 1200,
+            position: {
+              gravity: 'west'
+            }
+          },
+          {
+            text: {
+              text: product.name,
+              fontFamily: 'Source Sans Pro',
+              fontSize: 96,
+              fontWeight: 'bold'
+            },
+            crop: 'fit',
+            width: 920,
+            position: {
+              gravity: 'north_west',
+              x: 1340,
+              y: 140
+            }
+          }
+        ]}
+      />
 
       <Container>
         <div className={styles.productWrapper}>
